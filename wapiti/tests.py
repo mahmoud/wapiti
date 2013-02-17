8a08fcc4fe1cade7f130697b92a1c110a1a4adca
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-02-17 14:14:16 -0800

    oh and the afterbirthy tests

---
+++ wapiti/tests.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from wapiti import WapitiClient
+from operations.tests import call_and_ret
+
+
+def test_client_basic():
+    client = WapitiClient()
+    cat_pages = client.get_category_recursive('Africa', 1000)
+    return len(cat_pages) == 1000
+
+
+def main():
+    return call_and_ret(test_client_basic)
+
+
+if __name__ == '__main__':
+    print main()
